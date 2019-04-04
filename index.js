ab578ebcae9c550e80f87f9a58a35c83307e1347
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-04 17:04:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+class Meal {
+  
+}
+
+class Delivery {
+  
+}
+
+class Customer {
+  
+}
+
+class Neighborhood {
+  
+}

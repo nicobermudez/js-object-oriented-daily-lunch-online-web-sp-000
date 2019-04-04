d1e3605ac9aa068b8f05f40e7b63acb1fe10f2ed
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-04 17:04:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,17 +2,25 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
 class Meal {
+  constructor(name) {
 
+  }
 }
 
 class Delivery {
+  constructor(name) {
 
+  }
 }
 
 class Customer {
+  constructor(name) {
 
+  }
 }
 
 class Neighborhood {
-  
+  constructor(name) {
+
+  }
 }
